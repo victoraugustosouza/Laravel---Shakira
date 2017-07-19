a47d1911450a2856c3fd208d25e148c387adf028
joserobertoecjr <joserobertoecjr@gmail.com>
2017-07-19 02:35:26 -0300

    adicionando TODOs para fazer na aula

---
+++ app/Http/Controllers/ServicosController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicosController extends Controller
+{
+    //TODO: CRIAR UM METODO QUE RETORNE ALGUNS SERVICOS PRESTADOS
+
+    /*
+        PARA FACILITAR, VOCE PODE COPIAR E COLOCAR O ARRAY ABAIXO
+
+        $servicos = [
+                        [
+                            'nome' => 'Tiradentes',
+                            'descricao' => 'Tiramos seus dentes'
+                        ],
+                        [
+                            'nome' => 'Soco na cara',
+                            'descricao' => 'Tiramos seus dentes com um soco na cara'
+                        ],
+                        [
+                            'nome' => 'Carinho'
+                            'descricao' => 'Tiramos seus dentes com o maior carinho do mundo'
+                        ],
+                        [
+                            'nome' => 'Escondido',
+                            'descricao' => 'Voce nem percebe que tiramos seus dentes'
+                        ]
+                    ];
+
+    */
+}
---
+++ resources/views/servicos.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('title', 'Welcome')
+
+@section('corpo')
+
+    <div class="row">
+        <div class="col-xs-6 col-xs-offset-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Serviços</div>
+                <div class="panel-body">
+                    Veja os serviços que oferecemos.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- TODO: CRIAR UM FOREACH QUE EXIBA OS SERVICOS QUE VEM DA CONTROLLER AQUI -->
+
+@endsection
